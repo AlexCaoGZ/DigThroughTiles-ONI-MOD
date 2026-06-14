c941f39770f1ddca1e5b81707b85053b7caf3c92
Alex <82902418+AlexCaoGZ@users.noreply.github.com>
2026-06-14 04:48:36 -0400

    修复了挖掘背景墙会导致前景砖块损坏的问题

--- DigThroughTiles/Patch.cs
+++ DigThroughTiles/Patch.cs
@@ -25,8 +25,9 @@ namespace DigThroughTiles
                 bool flag2 = DigTool.Instance.IsActiveLayer(ToolParameterMenu.FILTERLAYERS.NATURALBACKWALL);
                 bool flag3 = Grid.Solid[cell] && !Grid.Foundation[cell];
 
-                // 在判定天然背景墙时，无视同一格中存在的其他建筑
+                // 去掉foundation和solid的判定，只检测背景墙
                 bool flag4 = BackwallManager.HasBackwall(cell);
+
                 if (Grid.Objects[cell, 7] == null && ((flag3 && flag) || (flag4 && flag2)))
                 {
                     for (int i = 0; i < 45; i++)
@@ -58,6 +59,72 @@ namespace DigThroughTiles
                 __result = null;
                 return false;
             }
+        }
+        [HarmonyPatch(typeof(Diggable), "OnSolidChanged")]
+        public class Diggable_OnSolidChanged_Patch
+        {
+            // 在 OnSolidChanged 执行前，临时把 Grid.Foundation 置为 false
+            // 如果 Grid.Foundation 是true的话，Diggable 会认为这个格子是有实体方块的，不允许挖背景墙
+            public static void Prefix(Diggable __instance, int ___cached_cell, out bool __state)
+            {
+                __state = false;
+                if (__instance.WillDigBackwall()
+                    && Grid.Solid[___cached_cell]
+                    && Grid.Foundation[___cached_cell]
+                    && BackwallManager.HasBackwall(___cached_cell))
+                {
+                    __state = true; // 临时修改标记
+                    Grid.Foundation[___cached_cell] = false;
+                }
+            }
+            public static void Postfix(int ___cached_cell, bool __state)
+            {
+                if (__state)
+                {
+                    Grid.Foundation[___cached_cell] = true; // 恢复标记
+                }
+            }
+        }
+    }
+    [HarmonyPatch(typeof(Diggable), "OnWorkTick")]
+    public class Diggable_OnWorkTick_Patch
+    {
+        public static bool Prefix(
+            Diggable __instance,
+            WorkerBase worker,
+            float dt,
+            ref bool __result,
+            int ___cached_cell,
+            ref bool ___isDigComplete)
+        {
+            // 拦截，挖掘格子里有玩家放置的实体方块
+            if (Grid.Solid[___cached_cell] && Grid.Foundation[___cached_cell])
+            {
+                // 挖掘的是背景墙
+                if (__instance.WillDigBackwall() && BackwallManager.HasBackwall(___cached_cell))
+                {
+                    // 只对背景墙造成伤害
+                    float approximateDigTime = Diggable.GetApproximateDigTime(___cached_cell);
+                    float amount = dt / approximateDigTime;
+                    float damage = Grid.Damage[___cached_cell];
+                    damage += amount;
+                    Grid.Damage[___cached_cell] = Mathf.Min(1f, damage);
+                    if (Grid.Damage[___cached_cell] >= 1f)
+                    {
+                        SimMessages.Dig(___cached_cell, -1, skipEvent: false, backwall: true);
+                        Grid.Damage[___cached_cell] = 0f;
+                    }
+                }
+                else
+                {
+                    // 挖的不是背景，标记任务完成
+                    ___isDigComplete = true;
+                }
+                __result = ___isDigComplete;
+
+                return false;
+            }
+            return true; // 其他情况走原版逻辑
         }
     }
 }
